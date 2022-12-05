b04e5f2926c6ca11b60662d16fca1a75926ef527
Manoj Kanthraj <57621925+ManojK573@users.noreply.github.com>
2022-12-05 15:38:08 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "my-EC2" {
   ami = "ami-01cae1550c0adea9c"
   instance_type = "t2.micro"
   key_name = "X21201188-AmzLinux"
-  vpc_security_group_ids = [aws_security_group.private-sg.id]
+  vpc_security_group_ids = [aws_security_group.tomcat-sg.id]
   associate_public_ip_address = true
   root_block_device {
     volume_type = "gp2"
