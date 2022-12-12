5432a1687dcedd10b7c2e44df6a145605d047edb
Manoj Kanthraj <57621925+ManojK573@users.noreply.github.com>
2022-12-12 12:22:59 +0000

    Update ContactForm.java

--- src/main/java/com/clouddevops/addressbook/addressbook/ContactForm.java
+++ src/main/java/com/clouddevops/addressbook/addressbook/ContactForm.java
@@ -29,7 +29,6 @@ public class ContactForm extends FormLayout {
     TextField lastName = new TextField("Last name");
     TextField phone = new TextField("Phone");
     TextField email = new TextField("Email");
-    DateField birthDate = new DateField("Birth date");
 
     Contact contact;
 
@@ -59,7 +58,7 @@ public class ContactForm extends FormLayout {
 
         HorizontalLayout actions = new HorizontalLayout(save, cancel);
         actions.setSpacing(true);
-        addComponents(actions, firstName, lastName, phone, email, birthDate);
+        addComponents(actions, firstName, lastName, phone, email);
     
     }
 
